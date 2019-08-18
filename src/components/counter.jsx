29f07f0be9a2e4aa09165e8726fde921c9a74ff5
eyakub <eyakubsorkar@gmail.com>
2019-08-18 15:14:58 +0600

    react components, custom className using bootstrap

---
+++ src/components/counter.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    // state that has the data that needs to the components
+    state = {
+        count: 0,
+        imageUrl: 'https://picsum.photos/200',
+    };
+
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold',
+    };
+
+    render() {
+        // let classes = this.getBadgeClasses();
+        return (
+            <div>
+                <span className={this.getBadgeClasses()} >{ this.formatCount() }</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        const x ='zero';
+        return count === 0 ? x : count;
+    }
+}
+
+export default Counter;
