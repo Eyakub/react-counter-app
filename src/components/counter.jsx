eb37b850c30d107e405f3f1a69f76dd43b2c89bf
eyakub <eyakubsorkar@gmail.com>
2019-08-19 18:40:50 +0600

    updating the state and inline onClick with parameters pass

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,17 +17,18 @@ class Counter extends Component {
 //       this.handleIncrement = this.handleIncrement.bind(this);
 //   }
 
-  handleIncrement = () => {
-    console.log("increment clicked", this);
-  }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1})
+  };
+
 
   render() {
-    // let classes = this.getBadgeClasses();
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={ () => this.handleIncrement({ product: 'hi'}) }
           className="btn btn-secondary btn-sm"
         >
           Increment
