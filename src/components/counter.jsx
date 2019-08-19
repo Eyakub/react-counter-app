f81766aee03b4ebd27b7191f9b45a611274671be
eyakub <eyakubsorkar@gmail.com>
2019-08-19 18:13:43 +0600

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,15 +13,22 @@ class Counter extends Component {
         fontWeight: 'bold',
     };
 
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There are no Tags!</p>
+
+        return <ul>
+                    { this.state.tags.map(tag => <li key={ tag }>{ tag }</li>)}
+                </ul>
+    }
+
     render() {
         // let classes = this.getBadgeClasses();
         return (
             <div>
                 <span className={this.getBadgeClasses()} >{ this.formatCount() }</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    { this.state.tags.map(tag => <li key={ tag }>{ tag }</li>)}
-                </ul>
+                { this.state.tags.length === 0 && 'Please create a new tag!'}
+                {this.renderTags()}
             </div>
         );
     }
